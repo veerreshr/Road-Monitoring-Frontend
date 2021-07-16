2416e2e9780f1160432dc4d832272a147af98ef3
veeresh <veerreshr@gmail.com>
2021-07-16 11:59:58 +0530

    .

--- src/Components/Accelerometer.jsx
+++ src/Components/Accelerometer.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import AlertComponent from './AlertComponent';
 
-function Accelerometer() {
+function AccelerometerComponent() {
+    const [error,setError]=useState("");
+    const [active,setActive]=useState(false);
 const [value,setValue]=useState({x:0,z:0})
-    const freq = 10;
     useEffect(()=>{
-let value=[];
         (async function(){
             try {
                 if ("Accelerometer" in window) {
                     const {state} = await navigator.permissions.query({
                         name: "accelerometer"
                     });
+                    console.log(state);
+                    debugger;
                     if (state !== "granted") {
-                        // console.warn("You haven't granted permission to use the Accelerometer sensor");
-                        // errorMessage.innerHTML = "Please grant the permissio to use sensors!";
-                        // alert("Please grant the permissio to use sensors!");
+                        console.warn("You haven't granted permission to use the Accelerometer sensor");
+                        setError("Please grant the permission to use sensors!");
+                        alert("Please grant the permission to use sensors!");
                         return;
                     }
     
-                    const acl = new Accelerometer({ frequency: freq });
+                    const acl = new Accelerometer({ frequency: 15 });
                     acl.addEventListener("activate", () => {
-                        // frequency.innerHTML = freq;
-                        // activated.innerHTML = "True";
+                        setActive(true);
                       });
-                      acl.addEventListener("error", (error) => {
-                        //   console.error(`Error: ${error.name}`);
-                        //   errorMessage.innerHTML = `Error: ${error.name}`;
+                      acl.addEventListener("error", (err) => {
+                          console.error(`Error: ${err.name}`);
+                          setError(`Error: ${err.name}`);
                       });
                       acl.addEventListener("reading", () => {
                         setValue({
@@ -33,37 +35,35 @@ let value=[];
                             z:acl.z
                         });
                       });
-                      setInterval(()=>{
-                        alert(value);
-                        value=[];
-                      },1000)
                       acl.start();
-    
                 }else{
                     console.error("Sorry! Your browser doesn't support usage of sensors.");
-                    errorMessage.innerHTML = "Sorry! Your browser doesn't support usage of sensors.";
+                    setError("Sorry! Your browser doesn't support usage of sensors.");
                     alert("Sorry! Your browser doesn't support usage of sensors.");
                 }
-            } catch (error) {
-                    // Handle construction errors.
-    if (error.name === 'SecurityError') {
-        console.log('Sensor construction was blocked by the Permissions Policy.');
-    } else if (error.name === 'ReferenceError') {
-        console.log('Sensor is not supported by the User Agent.');
-    } else {
-        throw error;
-    }
+            } catch (err) {
+                // Handle construction errors.
+                if (err.name === 'SecurityError') {
+                    console.log('Sensor construction was blocked by the Permissions Policy.');
+                } else if (err.name === 'ReferenceError') {
+                    console.log('Sensor is not supported by the User Agent.');
+                } else {
+                    throw err;
+                }
             }
-
         }());
+
     },[])
 
     return (
         <div>
-<p>X : {value.x}</p>
-<p>Z : {value.z}</p>
+            {error && <AlertComponent data={error}/>}
+            {active && <div>
+                <p>X : {value.x}</p>
+                <p>Z : {value.z}</p>
+                </div>}
         </div>
-    )
+    );
 }
 
-export default Accelerometer
+export default AccelerometerComponent
--- src/Components/ParameterTest.jsx
+++ src/Components/ParameterTest.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import SensorCard from './SensorCard';
 import Accelerometer from './Accelerometer';
 
 function ParameterTest() {
