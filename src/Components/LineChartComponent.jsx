eda49027d22a10bfd7202c48569879e45d8f853a
veeresh <veerreshr@gmail.com>
2021-07-17 05:55:17 +0530

    .

--- src/Components/LineChartComponent.jsx
+++ src/Components/LineChartComponent.jsx
@@ -1,215 +1,73 @@
-// import React, { useEffect,useState } from "react";
-// import {
-//   LineChart,
-//   Line,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-//   ResponsiveContainer,
-// } from "recharts";
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
+import  React,{ useState,useEffect }  from 'react';
+import zoomline from "fusioncharts/fusioncharts.zoomline";
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+ReactFusioncharts.fcRoot(FusionCharts,charts,zoomline,FusionTheme);
+// Resolves charts dependancy
+charts(FusionCharts);
 
 
 
-// export default function App({ values }) {
-//     const [data,setData]=useState()
-//   useEffect(() => {
-//     console.log(values);
-//     let data=[];
-//     for(let key in values){
-//         if (values.hasOwnProperty(key)) {
-//             data.push(values[key]);
-//         }
-//     }
-//     setData(data)
-//     // const data = [
-//     //     {
-//     //       timestamp: "Page A",
-//     //       x: 4000,
-//     //       z: 2400,
-//     //       amt: 2400,
-//     //     },
-//     //   ];
-//   }, [values]);
-//   return (
-//     <ResponsiveContainer width="100%" height={400}>
-//       <LineChart
-//         data={data}
-//         margin={{
-//           top: 50,
-//           right: 30,
-//           left: 20,
-//           bottom: 5,
-//         }}
-//       >
-//         <CartesianGrid strokeDasharray="3 3" />
-//         <XAxis dataKey="timestamp" />
-//         <YAxis />
-//         <Tooltip />
-//         <Legend />
-//         <Line
-//           type="monotone"
-//           dataKey="x"
-//           stroke="#8884d8"
-//           activeDot={{ r: 8 }}
-//         />
-//         <Line type="monotone" dataKey="z" stroke="#82ca9d" />
-//       </LineChart>
-//     </ResponsiveContainer>
-//   );
-// }
+function LineChartComponent({values}) {
 
+    const [config,setConfig]=useState();
 
-
-
-
-import React, { PureComponent } from 'react';
-import {
-  Label,
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ReferenceArea,
-  ResponsiveContainer,
-} from 'recharts';
-
-const initialData = [
-  { name: 1, cost: 4.11, cost2: 1 },
-  { name: 2, cost: 2.39, cost2: 1 },
-  { name: 3, cost: 1.37, cost2: 1 },
-  { name: 4, cost: 1.16, cost2: 1 },
-  { name: 5, cost: 2.29, cost2: 2 },
-  { name: 6, cost: 3, cost2: 4 },
-  { name: 7, cost: 0.53, cost2: 5},
-  { name: 8, cost: 2.52, cost2: 1 },
-  { name: 9, cost: 1.79, cost2: 2 },
-  { name: 10, cost: 2.94, cost2: 2 },
-  { name: 11, cost: 4.3, cost2: 2 },
-  { name: 12, cost: 4.41, cost2: 3 },
-  { name: 13, cost: 2.1, cost2: 5},
-  { name: 14, cost: 8, cost2: 1 },
-  { name: 15, cost: 0, cost2: 3 },
-  { name: 16, cost: 9, cost2: 4 },
-  { name: 17, cost: 3, cost2: 2 },
-  { name: 18, cost: 2, cost2: 5},
-  { name: 19, cost: 3, cost2: 1 },
-  { name: 20, cost: 7, cost2: 1 },
-];
-
-const getAxisYDomain = (from, to, ref, offset) => {
-  const refData = initialData.slice(from - 1, to);
-  let [bottom, top] = [refData[0][ref], refData[0][ref]];
-  refData.forEach((d) => {
-    if (d[ref] > top) top = d[ref];
-    if (d[ref] < bottom) bottom = d[ref];
-  });
-
-  return [(bottom | 0) - offset, (top | 0) + offset];
-};
-
-const initialState = {
-  data: initialData,
-  left: 'dataMin',
-  right: 'dataMax',
-  refAreaLeft: '',
-  refAreaRight: '',
-  top: 'dataMax+1',
-  bottom: 'dataMin-1',
-  top2: 'dataMax+20',
-  bottom2: 'dataMin-20',
-  animation: true,
-};
-
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/highlight-zomm-line-chart-v77bt';
-
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-  }
-
-  zoom() {
-    let { refAreaLeft, refAreaRight } = this.state;
-    const { data } = this.state;
-
-    if (refAreaLeft === refAreaRight || refAreaRight === '') {
-      this.setState(() => ({
-        refAreaLeft: '',
-        refAreaRight: '',
-      }));
-      return;
-    }
-
-    // xAxis domain
-    if (refAreaLeft > refAreaRight) [refAreaLeft, refAreaRight] = [refAreaRight, refAreaLeft];
-
-    // yAxis domain
-    const [bottom, top] = getAxisYDomain(refAreaLeft, refAreaRight, 'cost', 1);
-    const [bottom2, top2] = getAxisYDomain(refAreaLeft, refAreaRight, 'cost2', 50);
-
-    this.setState(() => ({
-      refAreaLeft: '',
-      refAreaRight: '',
-      data: data.slice(),
-      left: refAreaLeft,
-      right: refAreaRight,
-      bottom,
-      top,
-      bottom2,
-      top2,
-    }));
-  }
-
-  zoomOut() {
-    const { data } = this.state;
-    this.setState(() => ({
-      data: data.slice(),
-      refAreaLeft: '',
-      refAreaRight: '',
-      left: 'dataMin',
-      right: 'dataMax',
-      top: 'dataMax+1',
-      bottom: 'dataMin',
-      top2: 'dataMax+50',
-      bottom2: 'dataMin+50',
-    }));
-  }
-
-  render() {
-    const { data, barIndex, left, right, refAreaLeft, refAreaRight, top, bottom, top2, bottom2 } = this.state;
-
+    useEffect(()=>{
+          let timestamps="";
+          let x="";
+          let z="";
+        for(let key in values){
+            if(values.hasOwnProperty(key)){
+                timestamps+=values[key].timestamp+"|";
+                x+=values[key].x+"|";
+                z+=values[key].z+"|";
+            }
+        }
+        setConfig({
+            chart: {
+              caption: "Accelerometer vs Timestamp",
+              subcaption: "Click & drag on the plot area to zoom & then scroll",
+              yaxisname: "Acceleration(m/s2)",
+              xaxisname: "Timestamp",
+              forceaxislimits: "1",
+              pixelsperpoint: "0",
+              pixelsperlabel: "30",
+              compactdatamode: "1",
+              dataseparator: "|",
+              theme: "fusion"
+            },
+            categories: [
+              {
+                category:timestamps.slice(0,-1)
+                  
+              }
+            ],
+            dataset: [
+              {
+                seriesname: "x",
+                data:x.slice(0,-1)
+                  
+              },
+              {
+                seriesname: "z",
+                data:z.slice(0,-1)
+              }
+            ]
+          })
+    },[values])
     return (
-      <div className="highlight-bar-charts" style={{ userSelect: 'none', width: '100%' }}>
-        <button type="button" className="btn update" onClick={this.zoomOut.bind(this)}>
-          Zoom Out
-        </button>
-
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart
-            width={800}
-            height={400}
-            data={data}
-            onMouseDown={(e) => this.setState({ refAreaLeft: e.activeLabel })}
-            onMouseMove={(e) => this.state.refAreaLeft && this.setState({ refAreaRight: e.activeLabel })}
-            // eslint-disable-next-line react/jsx-no-bind
-            onMouseUp={this.zoom.bind(this)}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <YAxis orientation="right" allowDataOverflow domain={[bottom2, top2]} type="number" yAxisId="2" />
-            <Tooltip />
-            <Line  type="natural" dataKey="cost" stroke="#8884d8" animationDuration={300} />
-            <Line  type="natural" dataKey="cost2" stroke="#82ca9d" animationDuration={300} />
-
-            {refAreaLeft && refAreaRight ? (
-              <ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} />
-            ) : null}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    );
-  }
+        config?
+        <ReactFusioncharts
+        type="zoomline"
+        width="100%"
+        height="100%"
+        dataFormat="JSON"
+        dataSource={config}
+      />:<div></div>
+    )
 }
+
+export default LineChartComponent
