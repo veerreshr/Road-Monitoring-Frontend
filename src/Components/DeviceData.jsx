b1860b3d676d5e65a43655d875093b7f7f083b39
veeresh <veerreshr@gmail.com>
2021-07-27 03:41:38 +0530

    Update: Device Data layout

--- src/Components/DeviceData.jsx
+++ src/Components/DeviceData.jsx
@@ -1,74 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import AlertComponent from './AlertComponent';
+import React, { useEffect, useState } from "react";
+import AlertComponent from "./AlertComponent";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+red:{
+  background:"red"
+}
+});
 
 function DeviceData() {
-    const [values,setValue]=useState([]);
-    const [error, setError] = useState("");
-    const [active, setActive] = useState(false);
+  const classes = useStyles();
+  const [values, setValue] = useState([]);
+  const [error, setError] = useState("");
+  const [active, setActive] = useState(false);
 
-
-    const acl = new Accelerometer({ frequency: 30 });
-    const start = async () => {
-        try {
-          if ("Accelerometer" in window) {
-            const { state } = await navigator.permissions.query({
-              name: "accelerometer",
-            });
-            if (state !== "granted") {
-              console.warn(
-                "You haven't granted permission to use the Accelerometer sensor"
-              );
-              setError("Please grant the permission to use sensors!");
-              return;
-            }
-            acl.addEventListener("activate", () => {
-              setActive(true);
-            });
-            acl.addEventListener("error", (err) => {
-              console.error(`Error: ${err.name}`);
-              setError(`Error: ${err.name}`);
-            });
-    
-            acl.addEventListener("reading", () => {
-              setValue([acl.x,acl.z,acl.timestamp]);
-            });
-              acl.start();
-          } else {
-            console.error("Sorry! Your browser doesn't support usage of sensors.");
-            setError("Sorry! Your browser doesn't support usage of sensors.");
-          }
-        } catch (err) {
-          if (err.name === "SecurityError") {
-            console.warn(
-              "Sensor construction was blocked by the Permissions Policy."
-            );
-            setError("Sensor construction was blocked by the Permissions Policy.");
-          } else if (err.name === "ReferenceError") {
-            console.warn("Sensor is not supported by the User Agent.");
-            setError("Sensor is not supported by the User Agent.");
-          } else {
-            throw err;
-          }
+  const acl = new Accelerometer({ frequency: 30 });
+  const start = async () => {
+    try {
+      if ("Accelerometer" in window) {
+        const { state } = await navigator.permissions.query({
+          name: "accelerometer",
+        });
+        if (state !== "granted") {
+          console.warn(
+            "You haven't granted permission to use the Accelerometer sensor"
+          );
+          setError("Please grant the permission to use sensors!");
+          return;
         }
-      };
+        acl.addEventListener("activate", () => {
+          setActive(true);
+        });
+        acl.addEventListener("error", (err) => {
+          console.error(`Error: ${err.name}`);
+          setError(`Error: ${err.name}`);
+        });
 
-      useEffect(()=>{
-          start();
-          return()=>{
-              acl.stop();
-          }
-      },[])
-    return (
+        acl.addEventListener("reading", () => {
+          setValue([acl.x,acl.y, acl.z, acl.timestamp]);
+        });
+        acl.start();
+      } else {
+        console.error("Sorry! Your browser doesn't support usage of sensors.");
+        setError("Sorry! Your browser doesn't support usage of sensors.");
+      }
+    } catch (err) {
+      if (err.name === "SecurityError") {
+        console.warn(
+          "Sensor construction was blocked by the Permissions Policy."
+        );
+        setError("Sensor construction was blocked by the Permissions Policy.");
+      } else if (err.name === "ReferenceError") {
+        console.warn("Sensor is not supported by the User Agent.");
+        setError("Sensor is not supported by the User Agent.");
+      } else {
+        throw err;
+      }
+    }
+  };
+
+  useEffect(() => {
+    start();
+    return () => {
+      acl.stop();
+    };
+  }, []);
+  return (
+    <div>
+      {error && <AlertComponent data={error} />}
+      {active && (
+        <Card className={(values[2]>9.8 || values[2]<9.5)?classes.red:" "}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              Accelerometer Sensor Data
+            </Typography>
+            <Typography variant="body2" component="p">
+              X-axis Acceleration : {values[0]}
+              <br />
+              Y-axis Acceleration : {values[1]}
+              <br />
+              Z-axis Acceleration : {values[2]}
+              <br/>
+              Timestamp: {values[3]} 
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
+      {active && (
         <div>
-            {error && <AlertComponent data={error} />}
-            {active && <div>
-                X: {values[0]} <br />
-                Z: {values[1]} <br />
-                Timestamp: {values[2]} <br />
-                </div>}
-            
+          X: {values[0]} <br />
+          Z: {values[1]} <br />
+          Timestamp: {values[2]} <br />
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default DeviceData
+export default DeviceData;
